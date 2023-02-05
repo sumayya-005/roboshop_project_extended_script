ce54a8249a722df281bd81616a3191dae90a8296
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-05 11:03:55 +0530

    updating ./roboshop_project_extended_script

---
+++ frontend.sh
@@ -0,0 +1,38 @@
+LOG_FILE=/tmp/frontend
+
+
+echo Installing Nginx
+yum install nginx -y &>>$LOG_FILE
+StatusCheck $?
+
+
+cd /usr/share/nginx/html
+
+echo Removing Old Web Content
+rm -rf * &>>/tmp/frontend
+StatusCheck $?
+
+echo Extracting Web Content
+unzip /tmp/frontend.zip &>>$LOG_FILE
+StatusCheck $?
+
+#mv frontend-main/static/* . &>>$LOG_FILE
+#mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+
+#echo "Update RoboShop Config File"
+#for component in catalogue user cart payment shipping ; do
+#  sed -i -e "/$component/ s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+#done
+##sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+#StatusCheck $?
+
+echo Starting Nginx Service
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
+StatusCheck $?
+
+ if [ pgrep -x $SERVICE ]; then
+   echo "$SERVICE is running"
+ else
+   echo "$SERVICE is not running"
+ fi
