86a9cca8c709b5de0252579eafc5e4dcffa797ff
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-07 11:14:37 +0530

    updating ./roboshop_project_extended_script

--- nginx.sh
+++ nginx.sh
@@ -2,10 +2,25 @@
 
 echo "Checking nginx Installation"
 
-SERVICE="nginx"
-if  sudo yum list installed  | grep "$SERVICE">/dev/null
-then
+SERVICE=nginx
+yum list installed | grep ^$SERVICE &>/dev/null
+if [ $? -eq 0 ]; then
     echo "$SERVICE is installed"
 else
     echo "$SERVICE is not installed"
+    exit 1
+fi
+
+
+
+echo "CHECKING FILE CREATED"
+
+sleep 1
+
+file="/etc/nginx/default.d/roboshop.conf"
+if [ -f "$file" ]
+then
+        echo "$file created."
+else
+        echo "$file not created."
 fi
