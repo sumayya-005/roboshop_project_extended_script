98135e8140aef487683718e7a6da721628f0bb2e
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-09 00:06:56 +0530

    updating ./roboshop_project_extended_script

--- nginx.sh
+++ nginx.sh
@@ -42,3 +42,21 @@ else
   echo "service is not running"
 fi
 
+
+
+
+echo "CHECKING THE SSH CONNECTION"
+
+# cat /tmp/check_connectivity.sh
+#!/bin/bash
+
+server=10.10.10.10      # server IP
+port=22                 # port
+connect_timeout=5       # Connection timeout
+
+timeout $connect_timeout bash -c "</dev/tcp/$server/$port"
+if [ $? == 0 ];then
+   echo "SSH Connection to $server over port $port is possible"
+else
+   echo "SSH connection to $server over port $port is not possible"
+fi
